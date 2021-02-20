93ab1a4cc4ebc0de31dcb7f3587fc2968c1b96d5
Justin Yeoh <yeohjerwei@gmail.com>
2021-02-20 19:16:17 +0900

    Added get queue by user id

--- backend/models/queue/repository.py
+++ backend/models/queue/repository.py
@@ -3,6 +3,9 @@ from sqlalchemy import func
 from .model import Queue as QueueModel
 from .schema import Queue as QueueSchema
 
+def get_queue_by_user_id(db: Session, user_id: str):
+    return db.query(QueueModel).filter(QueueModel.user_id == user_id).all()
+
 def create_queue(db: Session, queue: QueueModel):
     db_queue = QueueSchema(**queue.dict())
     db.add(db_queue)
@@ -10,7 +13,7 @@ def create_queue(db: Session, queue: QueueModel):
     db.refresh(db_queue)
     return db_queue
 
-def update_queue(db: Session, queue: QueueModel):
+def update_queue_by_id(db: Session, queue: QueueModel):
     db_queue = (
         db.query(QueueModel).filter(QueueModel.id == queue.id).first().update(**queue.dict())
     )
